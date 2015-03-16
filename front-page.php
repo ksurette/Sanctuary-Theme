90566bd0f05c2321307814e1d411a03a70881098
ksurette <kimmsurette@gmail.com>
2015-03-15 21:25:49 -0400

    continuing editing

--- front-page.php
+++ front-page.php
@@ -9,8 +9,8 @@ get_header(); ?>
 <div class="home-background">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		a place to work
-            without distraction
+		<div class="home-text2">a place to work<br></div>
+            <div class="home-text1">without distraction</div>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
